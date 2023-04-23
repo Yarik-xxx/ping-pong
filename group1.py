38052b29fbd3296d3357dbd05b3b02d04858d2f0
Yarik_dev <99239824+Yarik-xxx@users.noreply.github.com>
2023-04-23 12:36:03 +0500

    Update group1.py

--- group1.py
+++ group1.py
@@ -1,4 +1,59 @@
+from random import randint
+
 from pygame import *
+
+
+# класс-родитель для других спрайтов
+class GameSprite(sprite.Sprite):
+    # конструктор класса
+    def __init__(self, player_image, player_x, player_y, size_x, size_y, player_speed):
+        # вызываем конструктор класса (Sprite):
+        sprite.Sprite.__init__(self)
+
+        # каждый спрайт должен хранить свойство image - изображение
+        self.image = transform.scale(image.load(player_image), (size_x, size_y))
+        self.speed = player_speed
+
+        # каждый спрайт должен хранить свойство rect - прямоугольник, в который он вписан
+        self.rect = self.image.get_rect()
+        self.rect.x = player_x
+        self.rect.y = player_y
+
+    # метод, отрисовывающий героя на окне
+    def reset(self):
+        window.blit(self.image, (self.rect.x, self.rect.y))
+
+
+# класс главного игрока
+class Player(GameSprite):
+    # метод для управления спрайтом стрелками клавиатуры
+    def update(self):
+        keys = key.get_pressed()
+        if keys[K_LEFT] and self.rect.x > 5:
+            self.rect.x -= self.speed
+        if keys[K_RIGHT] and self.rect.x < win_width - 80:
+            self.rect.x += self.speed
+
+
+# класс спрайта-врага
+class Enemy(GameSprite):
+    def __init__(self, player_image, player_x, player_y, size_x, size_y, player_speed, minus_if_skipping=True):
+        super().__init__(player_image, player_x, player_y, size_x, size_y, player_speed)
+        self.minus_if_skipping = minus_if_skipping
+
+    # движение врага
+    def update(self):
+        self.rect.y += self.speed
+        global lost
+        # исчезает, если дойдёт до края экрана
+        if self.rect.y > win_height:
+            self.rect.x = randint(80, win_width - 80)
+            self.rect.y = 0
+
+            # todo r
+            if self.minus_if_skipping:
+                lost = lost + 1
+
 
 # Игровая сцена:
 back = (200, 255, 255)  # цвет фона (background)
